824f97fb69740f8471537c59faf2781c2a7567f6
Mubashirshariq <mubashirsharik01@gmail.com>
2024-11-02 23:46:41 +0530

    added hamburger menu for smaller screens

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaHome, FaBookOpen, FaPlusCircle } from 'react-icons/fa';
+import { FaHome, FaBookOpen, FaPlusCircle, FaBars, FaTimes } from 'react-icons/fa';
 
 function Sidebar() {
     const location = useLocation();
-    console.log("location, ",location);
+    const [isOpen, setIsOpen] = useState(false); // State to manage the menu visibility
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className="fixed  top-24 min-h-screen z-100 navbar bg-gradient-to-r from-gray-900 to-gray-800 text-gray-200 p-4   shadow-lg">
-            <ul className="flex flex-col space-y-4 p-6">
+        <div className="fixed top-24 min-h-screen z-100 navbar bg-gradient-to-r from-gray-900 to-gray-800 text-gray-200 p-4 shadow-lg">
+            <button 
+                className="text-white block md:hidden"
+                onClick={toggleMenu} 
+            >
+                {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+            <ul className={`flex flex-col space-y-4 p-6 md:p-0 ${isOpen ? 'block' : 'hidden md:flex'}`}>
                 <li>
                     <Link 
                         to="/" 
